3f53625f603870085d4370f56d7ce0d52d48e3dd
Antoine Degonville <72013450+AntoineDegonville@users.noreply.github.com>
2021-01-08 12:33:17 +0100

    contact menu updated

--- src/components/ModalContact.js
+++ src/components/ModalContact.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "./ModalContact.css";
 import TextyAnim from "rc-texty";
+import linkedin from "../assets/img/linkedin.png";
+import instagram from "../assets/img/instagram.svg";
+import gmail from "../assets/img/gmail.svg";
 
 const ModalContact = ({ contactclicked, entered, contactclickedoff }) => {
   return (
@@ -30,48 +33,39 @@ const ModalContact = ({ contactclicked, entered, contactclickedoff }) => {
               RELATION
             </TextyAnim>
             <div className="descriptioncontainercontact">
-              <TextyAnim
-                className="description"
-                type="mask-top"
-                duration={(e) => {
-                  if (e.index === 8) {
-                    return 800;
-                  }
-                  return 1000;
-                }}
-                interval={(e) => {
-                  if (e.index === 2) {
-                    return 500;
-                  }
-                  return e.index * 30;
-                }}
-                onEnd={(type) => {
-                  // console.log(type);
-                }}
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://www.linkedin.com/in/antoine-degonville-ab65a9107/"
+                className="link"
               >
-                antoinedegonville.pro@gmail.com
-              </TextyAnim>
-              <TextyAnim
-                className="description"
-                type="mask-top"
-                duration={(e) => {
-                  if (e.index === 8) {
-                    return 800;
-                  }
-                  return 1000;
-                }}
-                interval={(e) => {
-                  if (e.index === 2) {
-                    return 500;
-                  }
-                  return e.index * 30;
-                }}
-                onEnd={(type) => {
-                  // console.log(type);
-                }}
+                <img
+                  alt="linkedin"
+                  rel="noreferrer"
+                  className="logolinkedin"
+                  src={linkedin}
+                ></img>
+              </a>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://www.instagram.com/antoinedegonville/"
+                className="link1"
               >
-                06 35 49 67 29
-              </TextyAnim>
+                <img
+                  alt="instagram"
+                  className="logoinstagram"
+                  src={instagram}
+                ></img>
+              </a>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="mailto:antoinedegonville.pro@gmail.com"
+                className="link2"
+              >
+                <img alt="gmail" className="logogmail" src={gmail}></img>
+              </a>
             </div>
           </div>
         </>
